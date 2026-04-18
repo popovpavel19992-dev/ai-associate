ba95d82a829ea63885f9ef67cdca0313c17de310
Fedor Kaspirovich <fedorkaspirovich@Andreis-MacBook-Pro.local>
2026-04-18 11:51:53 -0400

    feat: register research-enrich-statute Inngest stub for 2.2.3

--- src/server/inngest/index.ts
+++ src/server/inngest/index.ts
@@ -15,5 +15,6 @@ import { notificationReminders } from "./functions/notification-reminders";
 import { notificationOverdueCheck } from "./functions/notification-overdue-check";
 import { handlePortalNotification } from "./functions/handle-portal-notification";
 import { researchEnrichOpinion } from "./functions/research-enrich-opinion";
+import { researchEnrichStatute } from "./functions/research-enrich-statute";
 
-export const functions = [extractDocument, caseAnalyze, contractAnalyze, contractCompare, contractGenerate, creditReset, autoDelete, calendarEventSync, calendarSweep, calendarConnectionInit, calendarConnectionCleanup, teamMembershipCleanup, handleNotification, notificationReminders, notificationOverdueCheck, handlePortalNotification, researchEnrichOpinion];
+export const functions = [extractDocument, caseAnalyze, contractAnalyze, contractCompare, contractGenerate, creditReset, autoDelete, calendarEventSync, calendarSweep, calendarConnectionInit, calendarConnectionCleanup, teamMembershipCleanup, handleNotification, notificationReminders, notificationOverdueCheck, handlePortalNotification, researchEnrichOpinion, researchEnrichStatute];
